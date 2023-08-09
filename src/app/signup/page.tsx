1b0bb1e6ba2d0796dd26fc15a8f5e36eb4e2c8f5
Shahnawaz-Muhammad <shahnawazbaltee55@gmail.com>
2023-08-09 18:11:09 +0500

    signup api with db connection

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,14 +1,43 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
 export default function SignUp() {
+  const router = useRouter()
   const [user, setUser] = useState({
     username: "",
     email: "",
     password: "",
   });
+  const [buttonDisabled, setButtonDissabled] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const onSignup = async () => {
+    try {
+      setLoading(true)
+      console.log("Signup Successfull")
+
+      const response = await axios.post("api/users/signup", user)
+      console.log("Signup Successfull", response.data)
+      router.push("/login")
+    } catch (error:any) {
+      console.log("Signup Failed", error.message)
+    }
+    finally{
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0){
+      setButtonDissabled(false)
+    }
+    else{
+      setButtonDissabled(true)
+    }
+  }, [user])
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center gap-3">
@@ -55,8 +84,8 @@ export default function SignUp() {
       </div>
 
       <div className="flex flex-col gap-2 items-center">
-        <button className="px-4 py-2 bg-gray-800 text-white rounded-lg">
-          Submit
+        <button disabled={buttonDisabled} onClick={onSignup} className="px-4 py-2 bg-gray-800 text-white rounded-lg">
+          {loading ? "Loading..." : "Submit"}
         </button>
         <p>
           Already Signed up? <Link href="/login" className="underline italic">Login</Link>
--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,19 +1,24 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export async function connect() {
     try {
-        mongoose.connect(process.env.MONGO_URI!)
+        mongoose.connect(process.env.MONGO_URI!);
         const connection = mongoose.connection;
 
-        connection.on("connected", () => {
-            console.log("Mongodb Connected Successfully")
+        connection.on('connected', () => {
+            console.log('MongoDB connected successfully');
         })
 
-        connection.on("error", (err) => {
-            console.log("Mongodb Connected Error. Please make sure Mongodb is connected.", + err)
-            process.exit()
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+            process.exit();
         })
+
     } catch (error) {
-        console.log("Something went wrong", error)
+        console.log('Something goes wrong!');
+        console.log(error);
+        
     }
+
+
 }
